9a4370ead4a19e73c863acaa83c4d32f51e7d924
Andrew Santhosh <andrewsanthosh264@gmail.com>
2025-06-30 11:14:13 +0530

    let the homies with android 12, have some fun

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "com.andrew264.habits"
-        minSdk = 35
+        minSdk = 31
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
